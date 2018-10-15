a037ffbd2f0fc2ec127bcabfbee1624764782e44
mluk <maiklukasch@gmail.com>
2018-10-15 20:14:02 +0200

    fixing station position in config

--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,9 @@ export const config = {
 	SEA_LENGTH: 400,
 	INITIAL_SHIP_POSITION: [200, 200] as [number, number],
 	STATION: {
-		one: { position: [20, 200] as IVector2D, dockAngle: 0 },
-		two: { position: [380, 200] as IVector2D, dockAngle: 180 },
-		three: { position: [200, 20] as IVector2D, dockAngle: 90 },
-		four: { position: [200, 380] as IVector2D, dockAngle: 270 }
+		one: { position: [30, 200] as IVector2D, dockAngle: 0 },
+		two: { position: [370, 200] as IVector2D, dockAngle: 180 },
+		three: { position: [200, 30] as IVector2D, dockAngle: 90 },
+		four: { position: [200, 370] as IVector2D, dockAngle: 270 }
 	}
 };
