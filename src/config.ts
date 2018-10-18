eadcf9433dc880f1e89c87339d80cc042dc3ede5
mluk <maiklukasch@gmail.com>
2018-10-18 22:01:06 +0200

    increased loop interval in config

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import { IVector2D } from './utils/vectorUtils';
 
 export const config = {
 	SHIP_SPEED_PX_PER_SEC: 20,
-	UPDATE_LOOP_INTERVAL_MS: 25,
+	UPDATE_LOOP_INTERVAL_MS: 75,
 	INITIAL_SHIP_POSITION: [200, 200] as [number, number],
 	STATION: {
 		one: { stationNumber: 1, position: [30, 200] as IVector2D, dockAngle: 0 },
